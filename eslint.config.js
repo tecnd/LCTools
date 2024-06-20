84ce005c87035a4e60c857679da489d98baf8020
Kerry Wang <10476898+tecnd@users.noreply.github.com>
2024-06-20 00:50:35 -0500

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,27 @@
 import eslint from "@eslint/js";
 import eslintPluginSvelte from "eslint-plugin-svelte";
 import tseslint from "typescript-eslint";
+import svelteConfig from "./svelte.config.js";
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginSvelte.configs["flat/recommended"],
   ...eslintPluginSvelte.configs["flat/prettier"],
+  {
+    files: ["**/*.svelte", "*.svelte"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        svelteConfig,
+      },
+    },
+  },
+  {
+    rules: {
+      "svelte/no-at-html-tags": "warn",
+    },
+  },
   {
     ignores: [".svelte-kit/", "build/"],
   },
